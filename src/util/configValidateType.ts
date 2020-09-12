444986814131b90e959348f6b11c306948f171c6
Damy Metzke <damy.metzke@gmail.com>
2020-09-12 20:11:41 +0200

    added valicaters for a bunch of string types.

--- src/util/configValidateType.ts
+++ src/util/configValidateType.ts
@@ -1,4 +1,36 @@
-import { ConfigValidateFunction, ConfigValidateResult } from './configValidate';
+import { ConfigValidateFunction } from './configValidate';
+
+const REGEX_ALPHA = /^[a-zA-Z]*$/;
+const REGEX_NUMERIC = /^[0-9]*$/;
+const REGEX_ALPHA_NUMERIC = /^[a-zA-Z0-9]*$/;
+
+const REGEX_BINARY = /^(?:0b)[01]*$/;
+const REGEX_OCTAL = /^(?:0o)[0-7]*$/;
+const REGEX_HEXADECIMAL = /^(?:0x)[0-9abcdefABCCEF]*$/;
+
+function createRegexValidator(
+  test: RegExp,
+  successText: string,
+  failText: string,
+): ConfigValidateFunction {
+  return (value: any) => {
+    if (typeof value !== 'string') {
+      return {
+        success: false,
+        reason: 'value is not a string',
+      };
+    }
+    return test.test(value)
+      ? {
+        success: true,
+        reason: successText,
+      }
+      : {
+        success: false,
+        reason: failText,
+      };
+  };
+}
 
 export function exists(): ConfigValidateFunction {
   return (value: any) => (value === undefined
@@ -98,4 +130,50 @@ export function isNumberInRange({ min, max }:
       reason: `value is within range: ${rangeString}`,
     };
   };
+}
+
+export function isAlphaString(): ConfigValidateFunction {
+  return createRegexValidator(
+    REGEX_ALPHA,
+    'value is an alpha string',
+    'value is not an alpha string',
+  );
+}
+
+export function isNumericString(): ConfigValidateFunction {
+  return createRegexValidator(
+    REGEX_NUMERIC,
+    'value is a numeric string',
+    'value is not a numeric string',
+  );
+}
+
+export function isAlphaNumericString(): ConfigValidateFunction {
+  return createRegexValidator(
+    REGEX_ALPHA_NUMERIC,
+    'value is an alpha-numeric string',
+    'value is not an alpha-numeric string',
+  );
+}
+
+export function isBinaryString(): ConfigValidateFunction {
+  return createRegexValidator(
+    REGEX_BINARY,
+    'value is a binary string',
+    'value is not a binary string',
+  );
+}
+export function isOctalString(): ConfigValidateFunction {
+  return createRegexValidator(
+    REGEX_OCTAL,
+    'value is an octal string',
+    'value is not an octal string',
+  );
+}
+export function isHexadecimalString(): ConfigValidateFunction {
+  return createRegexValidator(
+    REGEX_HEXADECIMAL,
+    'value is a hexadecimal string',
+    'value is not a hexadecimal string',
+  );
 }
