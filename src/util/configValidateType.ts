e5551c538235f4ab28a09c915ce4a84ef8e2e455
Damy Metzke <damy.metzke@gmail.com>
2020-09-12 17:46:10 +0200

    added validate function: - isNumberInRange

--- src/util/configValidateType.ts
+++ src/util/configValidateType.ts
@@ -46,6 +46,7 @@ export function isNoneOf(firstValue: any, ...values: any[]): ConfigValidateFunct
   const itemsString = `[${String(firstValue)}${
     values.reduce((total, value) => `${total}, ${value}`, '')
   }]`;
+
   return (value: any) => {
     if ([firstValue, ...values].every((testValue) => value === testValue)) {
       return {
@@ -56,6 +57,45 @@ export function isNoneOf(firstValue: any, ...values: any[]): ConfigValidateFunct
     return {
       success: false,
       reason: `value is part of unexpected items: ${itemsString}`,
+    };
+  };
+}
+
+export function isNumberInRange({ min, max }:
+    {min: number, max: number}
+    | {min: number, max: undefined}
+    | {min: undefined, max: number}): ConfigValidateFunction {
+  const rangeString = `${
+    min === undefined
+      ? ''
+      : `${min} >= `
+  }n${
+    max === undefined
+      ? ''
+      : ` < ${max}`
+  }`;
+
+  return (value: any) => {
+    if (typeof value !== 'number') {
+      return {
+        success: false,
+        reason: 'value is not a number',
+      };
+    }
+
+    if (
+      (min !== undefined && value < min)
+        || (max !== undefined && value >= max)
+    ) {
+      return {
+        success: false,
+        reason: `value is not within range: ${rangeString}`,
+      };
+    }
+
+    return {
+      success: true,
+      reason: `value is within range: ${rangeString}`,
     };
   };
 }
