10bd34651b7f8309ea410a109fe0c081aa629a57
Damy Metzke <damy.metzke@gmail.com>
2020-09-14 21:34:34 +0200

    added sassConfigValidate.

--- src/external/sass/sassConfigValidate.ts
+++ src/external/sass/sassConfigValidate.ts
@@ -1,12 +1,12 @@
 import { ConfigValidateObject, validate } from '../../config/index';
 
 export const SASS_VALIDATE: ConfigValidateObject = {
-  config: {
-    sourceMap: validate.isOfType('boolean'),
-    outputStyle: validate.isAnyOf('expanded', 'compressed'),
-    subFolder: validate.isOfType('string'), // todo: check for valid path
-    singleFolder: validate.isOfType('boolean'),
-    maxDepth: validate.isNumberInRange({ min: 0, max: undefined }),
+  options: {
+    sourceMap: validate.optionally(validate.isOfType('boolean')),
+    outputStyle: validate.optionally(validate.isAnyOf('expanded', 'compressed')),
+    subFolder: validate.optionally(validate.isOfType('string')), // todo: check for valid path
+    singleFolder: validate.optionally(validate.isOfType('boolean')),
+    maxDepth: validate.optionally(validate.isNumberInRange({ min: 0, max: undefined })),
   },
   input: validate.isArrayOf(validate.isOfType('string')), // todo: check for valid paths
   output: validate.isOfType('string'), // todo: check for valid paths
