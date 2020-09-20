2e84bc6236141e27cda317f1a2bbcb9ec041475b
Damy Metzke <damy.metzke@gmail.com>
2020-09-20 14:29:13 +0200

    added filterByName to ErrrorCollection.

--- src/error/errorCollection.ts
+++ src/error/errorCollection.ts
@@ -1,6 +1,10 @@
 import * as _ from 'lodash';
 
 export class ErrorCollection extends Error {
+    static ERROR_NAME = 'CollectionError';
+
+    static ALL_ERROR_NAMES = new Set([ErrorCollection.ERROR_NAME])
+
     latest?: Error;
 
     errors: Error[];
@@ -15,7 +19,7 @@ export class ErrorCollection extends Error {
       this.latest = _.last(errors);
       this.errors = errors.slice(0, -1);
 
-      this.name = 'CollectionError';
+      this.name = ErrorCollection.ERROR_NAME;
     }
 
     push(error: Error): void {
@@ -47,5 +51,21 @@ export class ErrorCollection extends Error {
         : [...other.errors, other.latest];
 
       return new ErrorCollection([...thisErrors, ...otherErrors]);
+    }
+
+    filterByName(name: string): ErrorCollection {
+      const thisErrors = typeof this.latest === 'undefined'
+        ? []
+        : [...this.errors, this.latest];
+
+      const filteredErrors = thisErrors.filter((error) => {
+        const errorPrototype = Object.getPrototypeOf(error);
+        return ('ALL_ERROR_NAMES' in errorPrototype)
+          ? (<Set<string>>errorPrototype.ALL_ERROR_NAMES)
+            .has(name)
+          : error.name === name;
+      });
+
+      return new ErrorCollection(filteredErrors);
     }
 }
