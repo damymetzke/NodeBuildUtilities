8afe9bec9cb0c36841ba69bac1797305bb47e0c6
Damy Metzke <damy.metzke@gmail.com>
2020-09-28 22:25:27 +0200

    converted UnknownError to use BuildError.

--- src/error/unknownError.ts
+++ src/error/unknownError.ts
@@ -1,17 +1,38 @@
-export class UnknownError extends Error {
-    static ERROR_NAME = 'unknown-error';
+import { BuildError, ISerializedError } from './buildError';
+import { registerDeserializer } from './deserializer';
 
-    static ALL_ERROR_NAMES = new Set([
+interface ISerializedUnknownError
+{
+  message: string
+}
+
+export class UnknownError extends BuildError {
+    static readonly ERROR_NAME = 'unknown-error';
+
+    static readonly ALL_ERROR_NAMES = new Set([
       UnknownError.ERROR_NAME,
     ])
 
     constructor(val: unknown) {
-      if (typeof val === 'string') {
-        super(`recieved incorrect error string with value: ${val}`);
-      } else {
-        super(`recieved incorrect error of type ${typeof val}`);
-      }
+      super('recieved incorrect error string:\n'
+      + `\t                 type:${typeof val}\n`
+      + `\tstring representation: ${String(val)}`);
 
       this.name = UnknownError.ERROR_NAME;
     }
+
+    serialize(): ISerializedError {
+      return <ISerializedError<ISerializedUnknownError>>{
+        type: UnknownError.ERROR_NAME,
+        error: {
+          message: this.message,
+        },
+      };
+    }
 }
+
+registerDeserializer(UnknownError.ERROR_NAME, (serialized: ISerializedUnknownError) => {
+  const result = new UnknownError(undefined);
+  result.message = serialized.message;
+  return result;
+});
