11f68972c83a19e28d18b825f797648ecd5a6e0c
Damy Metzke <damy.metzke@gmail.com>
2020-09-29 18:13:44 +0200

    improve runScript. return value is converted and thrown if an error.

--- src/script/convertReturn.ts
+++ src/script/convertReturn.ts
@@ -1,11 +1,10 @@
 import { SubProcessError, UnknownError } from '../error';
 
 export async function ConvertReturn(
-  internal: (...args: unknown[])=>unknown,
-  args: unknown[],
+  internal: ()=>unknown,
 ): Promise<SubProcessError | void> {
   try {
-    const result = await Promise.resolve(internal(...args));
+    const result = await Promise.resolve(internal());
 
     if (result instanceof SubProcessError) {
       return result;
